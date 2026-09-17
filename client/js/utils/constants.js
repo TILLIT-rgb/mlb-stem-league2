ee944d08d2ac555cda1359805b9641a4c72e5f64
TILLIT-rgb <hg3230203@hiroogakuen.com>
2026-09-17 16:15:30 +0900

    Add WHEEL_SEQ constant to constants.js

--- client/js/utils/constants.js
+++ client/js/utils/constants.js
@@ -3,3 +3,4 @@ const OUTCOME_COLORS = ['#2ecc71','#f1c40f','#e67e22','#e74c3c','#3498db','#9b59
 const OUTCOME_LABELS_EN = ['Single','Double','Triple','Home Run','Walk','Hit By Pitch','Strikeout','Fly Out','Ground Out'];
 const OUTCOME_LABELS_JA = ['単打','二塁打','三塁打','ホームラン','四球','死球','三振','ファウルアウト','ゴロアウト'];
 const POSITIONS_UNIQUE = ['C','1B','2B','3B','SS'];
+const WHEEL_SEQ = [8,0,1,2,3,7,4,6,5];
